803341d06829d0f098cb35e4c7158bbd0e10b520
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-17 08:27:53 -0700

    fix case if index not 0

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
+		(*head)->prev->next = (*head)->next;
+		tmp = (*head)->prev;
 		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
+		if (tmp->next)
+			tmp->next->prev = tmp;
 		*head = saved_head;
 	}
 	return (1);
