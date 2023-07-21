e571098b34fe5384ec273bf92d054ce0a92f6994
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-21 14:38:52 -0700

    fix code of class user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -21,6 +21,7 @@ class User():
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
+        self.password = None
 
     @property
     def password(self):
@@ -40,7 +41,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -52,9 +53,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
@@ -84,7 +85,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
