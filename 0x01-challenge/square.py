567cf9e2843afbf91e5870b4b6d8fe339859c295
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-17 15:42:04 +0300

    square class fix perimeter of it

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
+""" module of class Square"""
+
 
 class square():
-    
+    """ class with squarwith two class attr """
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ init function docstring"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ perimeter of square docstring"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ magic method docstring """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
